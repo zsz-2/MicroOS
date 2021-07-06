ae0d8460fd698061ddc532ced2e5f75d1f965bc8
hwg-maker <1986716843@qq.com>
2021-07-06 18:36:17 +0800

    保护模式进阶

---
+++ code/c5/c/boot/WriteMAndL.sh
@@ -0,0 +1,4 @@
+nasm -I  include/ -o mbr.bin mbr.S &&
+nasm -I include/ -o loader.bin  loader.S &&
+dd if=mbr.bin of=/usr/share/bochs/hd60M.img bs=512 count=1 conv=notrunc &&
+dd if=loader.bin of=/usr/share/bochs/hd60M.img bs=512 count=4 seek=2 conv=notrunc
